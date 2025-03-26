0a9c09296350953dd4f120f33bd800b4ef009ab6
nineeko <gkxm1013@naver.com>
2025-03-26 14:48:01 +0900

    25.03.26 pm 2:47

--- chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestMapper.java
+++ chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestMapper.java
@@ -4,4 +4,8 @@ import java.util.List;
 
 public interface ElementTestMapper {
     List<MenuDTO> selectResultMapTest();
+
+    List<MenuAndCategoryDTO> selectResultMapAssociationTest();
+
+    List<CategoryAndMenuDTO> selectResultMapCollectionTest();
 }
--- chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
+++ chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
@@ -21,10 +21,28 @@ public class ElementTestService {
     }
 
     public void selectResultMapAssociationTest() {
+        SqlSession sqlSession = getSqlSession();
+        ElementTestMapper mapper = sqlSession.getMapper(ElementTestMapper.class);
 
+        List<MenuAndCategoryDTO> menuList = mapper.selectResultMapAssociationTest();
+        if(menuList != null && !menuList.isEmpty()) {
+            menuList.forEach(System.out::println);
+        }else{
+            System.out.println("조회 결과 없음");
+        }
+        sqlSession.close();
     }
 
     public void selectResultMapCollectionTest() {
+        SqlSession sqlSession = getSqlSession();
+        ElementTestMapper mapper = sqlSession.getMapper(ElementTestMapper.class);
 
+        List<CategoryAndMenuDTO> categoryList = mapper.selectResultMapCollectionTest();
+        if(categoryList != null && !categoryList.isEmpty()) {
+            categoryList.forEach(System.out::println);
+        }else{
+            System.out.println("조회 결과 없음");
+        }
+        sqlSession.close();
     }
 }
